66bac625dacd57cc451270a0e047182cc3f54c44
zhaoguiyou1 <740670684@qq.com>
2018-03-27 17:09:52 +0800

    Controller与 View之间的值传递
    使用View数据

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -16,6 +17,12 @@ namespace WebApplication1.Controllers
 
         public ActionResult GetView()
         {
+           
+            Employee emp = new Employee();
+            emp.FirstName = "Sukesh";
+            emp.LastName = "Marla";
+            emp.Salary = 20000;
+            ViewData["Employee"] = emp;
             return View("MyView");
         }
     }
