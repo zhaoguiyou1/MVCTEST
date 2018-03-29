f764bb0dac41a3abf6d901ee961b379c272f4017
zhaoguiyou1 <740670684@qq.com>
2018-03-29 11:30:52 +0800

    自定义服务器端验证
    自建类重写IsValid方法

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,11 +6,30 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
+    public class FirstNameValidation:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value ==null)
+            {
+                return new ValidationResult("Please Provide First Name");
+            }
+            else
+            {
+                if (value.ToString().Contains("@"))
+                {
+                    return new ValidationResult("First Name should contain @");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+
     public class Employee
     {
         [Key]
         public int EmployeeId { get; set; }
-        [Required(ErrorMessage = "Enter First Name")]
+        [FirstNameValidation]
         public string FirstName { get; set; }
         [StringLength(5, ErrorMessage = "Last Name length should not be greater than 5")]
         public string LastName { get; set; }
